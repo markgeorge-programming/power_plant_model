4e7dd231e0603dea02fbb83bac2fcf176995ebe0
markgeorge-programming <markgeo2005@gmail.com>
2026-08-31 17:33:48 +0300

    intial commit

--- app.py
+++ app.py
@@ -1,39 +1,35 @@
 from flask import Flask, render_template,request
-from tensorflow.keras.models import load_model
+import numpy as np
 import joblib
+import math
 
-
-model = load_model('addiction_model.h5')
-scaler = joblib.load('scaleraddiction.pkl')
+model = joblib.load("model.pkl")
+scaler = joblib.load('scaler.pkl')
 app = Flask(__name__)
 
+def round_up(value, decimals=0):
+  factor = 10**decimals
+  return math.ceil(value * factor) / factor
+
+
+print(round_up(4.121, 2))
 
 @app.route('/',methods=['GET', 'POST'])
 def hello_world():
     prediction = None
     if request.method == 'POST':
+        irridation=float(request.form.get('IRRADIATION'))
         # EXACT COLUMN MATCH PROTOCOL ENGAGED
         data = [
-            float(request.form.get('Daily_Usage_Hours')),
-            float(request.form.get('Phone_Checks_Per_Day')),
-            float(request.form.get('Apps_Used_Daily')),
-            float(request.form.get('Time_on_Social_Media')),
-            float(request.form.get('Time_on_Gaming')),
-            float(request.form.get('Sleep_Hours'))
+            float(request.form.get('AMBIENT_TEMPERATURE')),
+            irridation,
+            float(request.form.get('MODULE_TEMPREATURE'))* irridation,
         ]
-        i=0
-        while i < len(data):
-            data[i]=data[i]/100
-            i=i+1
+        raw_input = np.array(data).reshape(1, -1)
+        print(raw_input)
 
-        # Scale and predict
-        data_scaled = scaler.transform([data])
-
-        # Extract the raw Sigmoid probability (e.g., 0.854321...)
-        raw_prediction = model.predict(data_scaled)[0][0]
-
-        # The S-Rank Percentage Translation
-        prediction = int(round(raw_prediction * 100))
+        prediction_arr = model.predict(raw_input)
+        prediction = round_up(prediction_arr[0],2)
 
     return render_template('index.html', prediction=prediction)
 
